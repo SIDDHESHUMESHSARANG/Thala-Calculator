01f52f0125ea6f1c755a352167cfa7204e0e0cbf
Siddhesh Umesh Sarang <siddheshsarang0811@gmail.com>
2024-11-29 16:05:36 +0530

    Update main.py
    Some new updates with proper reasons for individual outputs.

--- main.py
+++ main.py
@@ -5,23 +5,31 @@ def check_input(userInput):
     userInput = userInput.lower()
 
     if len(userInput) == 7:
+        print("Your inputted string is 7 Characters long. Hence,")
         print("THALA FOR A REASON")
     elif "seven" in userInput:
+        print("Your Inputted String has word 'Seven'. Hence,")
         print("THALA FOR A REASON")
     elif "7" in userInput:
+        print("Your Inputted String has Number '7'.Hence,")
         print("THALA FOR A REASON")
     elif sum(ord(c) for c in userInput) == 7:
+        print("Your inputted String has 7 ASCII Value. Hence,")
         print("THALA FOR A REASON")
     elif len(userInput.split()) == 7:
+        print("Your inputted String has exactly 7 words. Hence,")
         print("THALA FOR A REASON")
     elif "dhoni" in userInput:
+        print("Your inputted String has 'Dhoni'. Hence,")
         print("THALA FOR A REASON")
     elif "ms" in userInput:
+        print("Your inputted String has 'MS'. Hence,")
         print("THALA FOR A REASON")
     elif "thala" in userInput:
+        print("Your inputted String has 'THALA'. Hence,")
         print("THALA FOR A REASON")
     else:
-        print("NO THALA FOUND, KINDLY ENTER A DIFFERENT STRING")
+        print("OoPs!\nNO THALA FOUND, KINDLY ENTER A DIFFERENT STRING")
 userInput = input("ENTER THE STRING HERE: ")
 check_input(userInput) 
 
